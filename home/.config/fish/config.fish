d88107902bc6e54c65e79f03b643362e5d1d0b92
cleversoap <clever@cleversoap.com>
2013-04-18 14:14:35 +1000

    Updated path and fixed colouring of user name in fish prompt

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -14,8 +14,13 @@ end
 
 function fish_prompt
 	if test -d .git
-		printf '%s%s@%s%s%s:%s$ ' (set_color purple) (whoami) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
+		printf '%s%s%s@%s%s%s:%s$ ' (set_color purple) (whoami) (set_color normal) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
 	else
-		printf '%s%s@%s%s%s$ ' (set_color purple) (whoami) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+		printf '%s%s%s@%s%s%s$ ' (set_color purple) (whoami) (set_color normal) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 	end
 end
+
+set fish_greeting ""
+set BROWSER 'open'
+set PATH /usr/local/bin /usr/local/sbin ~/bin /usr/local/share/npm/bin /usr/local/share/aclocal /opt/local/bin /opt/local/sbin /sbin /usr/local/Cellar/fishfish/OpenBeta_r2/bin /usr/X11R6/bin /usr/bin /bin /usr/sbin /sbin opt/X11/bin /usr/local/Cellar/flex_sdk/4.6.0.23201/libexec/bin /Users/mattclever/Code/depot_tools
+
