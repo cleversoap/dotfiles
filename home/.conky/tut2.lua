a4398ec5f7c1c8df07a9b7ce14fb2f606d74b60c
cleversoap <clever@cleversoap.com>
2013-09-25 15:18:38 +1000

    Now center aligning in .conkyrc and only drawing the arcs in the lua

--- home/.conky/tut2.lua
+++ home/.conky/tut2.lua
@@ -7,15 +7,10 @@ red,green,blue,alpha=1,1,1,1
 font_slant = CAIRO_FONT_SLANT_NORMAL
 font_face = CAIRO_FONT_WEIGHT_NORMAL
 
-function draw_arc(cr, text, percent, x, y)
+function draw_arc(cr, percent, x, y)
     if percent ~= nil then
-        -- Text
-        cairo_move_to(cr, x, y)
-        cairo_show_text(cr, text)
-        cairo_stroke(cr)
-
         -- Arc
-        cairo_arc(cr, x+13, y-3, 17, 0, ((percent/100)*360) * (math.pi/180))
+        cairo_arc(cr, x, y, 22, 0, ((percent/100)*360) * (math.pi/180))
         cairo_stroke(cr)
     end
 end
@@ -32,11 +27,10 @@ function conky_main()
 
     -- Vars
     cpu = tonumber(conky_parse("${cpu}"))
-    mem = conky_parse("${mem}")
     mem_perc = tonumber(conky_parse("${memperc}"))
 
-    draw_arc(cr, mem, mem_perc, 20, 35)
-    draw_arc(cr, cpu .. "%", cpu, 20, 83)
+    draw_arc(cr, mem_perc, 30, 35)
+    draw_arc(cr, cpu, 30, 90)
 
     -- Cleanup
     cairo_destroy(cr)
