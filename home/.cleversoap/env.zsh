d777675d196748213a2409356cc383674512ebc8
cleversoap <clever@cleversoap.com>
2016-03-21 10:53:42 +1000

    Will now simply update all plugins rather than needing to specify them. Automatically activates pyenv-virtualenv if it is found

--- home/.cleversoap/env.zsh
+++ home/.cleversoap/env.zsh
@@ -11,52 +11,51 @@ function clever_env {
 
     if [[ -d $HOME/.$1 ]]; then
 
+        # Get the *env root
         local env_root=$HOME/.$1
         export ${1:u}_ROOT=$env_root
         export PATH=$env_root/bin:$PATH
         eval "$($1 init -)"
         export ENV_PATH=$env_root:$ENV_PATH
 
-        if [[ ! -z $2 ]]; then
-            export ENV_BUILD_PLUGIN_PATH=$env_root/plugins/$2:$ENV_BUILD_PLUGIN_PATH
+        # Get plugins associated with it and update them too
+        if [[ -d $env_root/plugins ]]; then
+            for plugin in $env_root/plugins/*; do
+                export ENV_PLUGINS=$plugin:$ENV_PLUGINS
+            done
         fi
 
     fi
+
 }
 
-# This function, when called, will update all the env
-# functions and updaters
+# This function, when called, will update environments as well as all plugins
 function clever_env_update {
-
     clever_exec_paths $ENV_PATH 'clever_repo_update .'
-    clever_exec_paths $ENV_BUILD_PLUGIN_PATH 'clever_repo_update .'
-
-
-#    a=(${=${(s/:/)ENV_PATH}})
-#    
-#    for i in $a; do
-#        clever_repo_update ${i}
-#    done
+    clever_exec_paths $ENV_PLUGINS 'clever_repo_update .'
 }
 
 #-------------------------------------------------------------------------------
 
 # Clear exported env variables
 ENV_PATH=
-ENV_BUILD_PLUGIN_PATH=
+ENV_PLUGINS=
 
 # rbenv
-clever_env rbenv ruby-build
+clever_env rbenv
 
 # pyenv
-# NB: pyenv comes with python-build but we'll update it all the same
-clever_env pyenv python-build
+clever_env pyenv
+
+if [[ -d $HOME/.pyenv/plugins/pyenv-virtualenv ]]; then
+    eval "$(pyenv virtualenv-init -)"
+fi
 
 # luanv
-clever_env luaenv lua-build
+clever_env luaenv
 
 # nodenv
-clever_env nodenv node-build
+clever_env nodenv
 
 # jenv
 clever_env jenv
